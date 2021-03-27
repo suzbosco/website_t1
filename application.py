f11beab9e2d7f8cad811e5f1253fb24ae604cac7
suzbosco <73774716+suzbosco@users.noreply.github.com>
2021-03-28 03:44:58 +0800

    Update application.py

--- application.py
+++ application.py
@@ -14,17 +14,17 @@ def home():
 #def home(name):
  #   return render_template("index.html", content=name, t1=["bosco", "hugo", "on9jai"])
 
-#@application.route('/test')
-#def home1():
- #   return render_template("new.html")
+@application.route('/test')
+def home1():
+    return render_template("new.html")
 
 
 
 
-#redirect
-#@application.route('/admin')
-#def admin():
- #   return redirect(url_for("home1"))
+redirect
+@application.route('/admin')
+def admin():
+    return redirect(url_for("home1"))
 
 if __name__ == "__main__":
     application.run()
