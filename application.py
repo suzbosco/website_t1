d618882755335a18349619b85916b5505702db6c
suzbosco <73774716+suzbosco@users.noreply.github.com>
2021-03-30 13:30:11 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request
+from datetime import timedelta
+import os
 
 application = Flask(__name__, template_folder='templates', static_folder='img')
 app = application
+app.secret_key = os.urandom(24)
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 #main page
@@ -18,9 +22,39 @@ def home():
 def home1():
     return render_template("new.html")
 
-@application.route('/login')
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user_session"] = user
+        flash("Login Successful")
+        return redirect(url_for("user_fun"))
+    else:
+        if "user_session" in session:
+            flash("Already login")
+            return redirect(url_for("user_fun"))
+
+        return render_template("login.html")
+    
+@app.route("/user")
+def user_fun():
+    if "user_session" in session:
+        user = session["user_session"]
+        return render_template("user.html", user=user)
+    else:
+        flash("you are not login!")
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    if "user_session" in session:
+        user = session["user_session"]
+        flash(f"you have been logged out, {user}", "info")
+    session.pop("user_session", None)
+
+    return redirect(url_for("login"))
 
 
 
